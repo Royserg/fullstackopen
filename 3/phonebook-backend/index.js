c1eaa1c24e9c7b57ae798f807c34e5571171e84d
Jakub <staniojakub@gmail.com>
2019-10-24 09:25:44 +0200

    Enable cors

--- 3/phonebook-backend/index.js
+++ 3/phonebook-backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 const PORT = 3001
 
+// Enable cors
+app.use(cors())
 app.use(express.json())
 
 morgan.token('data', (req, res) => {
