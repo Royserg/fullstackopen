023dca23b7881775bed2bfdaaafbadd60fa5060e
Jakub <staniojakub@gmail.com>
2019-12-26 18:47:21 +0100

    Step 13: move mongoose config into module

---
+++ 3/phonebook-backend/models/person.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB', error.message)
+  })
+
+// Define schema for Person
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+personSchema.methods.toJSON = function () {
+  const user = this
+  const userObj = user.toObject()
+
+  userObj.id = userObj._id.toString()
+  delete userObj._id
+  delete userObj.__v
+
+  return userObj
+}
+
+module.exports = mongoose.model('Person', personSchema);
