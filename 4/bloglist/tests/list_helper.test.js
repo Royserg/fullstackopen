dffe924af7c9dd6a293fd4d755dbfc89f182c3ea
Jakub <staniojakub@gmail.com>
2020-01-03 13:59:11 +0100

    4.3: dummy helper function

---
+++ 4/bloglist/tests/list_helper.test.js
@@ -0,0 +1,9 @@
+/* eslint-env jest */
+const { dummy } = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = dummy(blogs)
+  expect(result).toBe(1)
+})
